ca95b4692181388a2ae792b4fc138fce5195eb17
Schlueter <bs@schlueter.blue>
2017-05-22 00:16:22 -0400

    Add gpg mgmt to keychain init

--- zshrc.d/11-keychain.zsh
+++ zshrc.d/11-keychain.zsh
@@ -1,2 +1,5 @@
+GPG_TTY=$(tty)
+export GPG_TTY
+eval $(gpg /bye)
 export GPG_AGENT_INFO="~/.gnupg/S.gpg-agent:$(pgrep gpg-agent):1"
-eval `keychain --eval --agents gpg,ssh id_ed25519 8C4854C3 --quiet`
+eval $(keychain --eval --agents gpg,ssh id_ed25519 8C4854C3 --quiet)
