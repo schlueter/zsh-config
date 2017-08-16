ceefd0dc366bad52e3605a0db7cc5de988894463
Schlueter <bs@schlueter.blue>
2017-08-16 11:23:40 -0400

    Add gre=git_recent_branches alias

--- zshrc.d/10-setup-aliases.zsh
+++ zshrc.d/10-setup-aliases.zsh
@@ -88,7 +88,6 @@ alias grbi='git              rebase --interactive'
 alias grbs='git              rebase --skip'
 alias grbu='git fetch && git rebase origin/$(git_primary_branch_base)'
 ## reset
-alias gre='git  reset'
 alias grh='git  reset HEAD'
 alias grhh='git reset HEAD --hard'
 ## rm
@@ -102,6 +101,8 @@ alias gt='git  stash'
 alias gtd='git stash drop'
 alias gtp='git stash pop'
 alias gts='git stash show'
+## misc
+alias gre=git_recent_branches
 
 ## Hub aliases
 alias gpr='git pull-request'
@@ -188,8 +189,13 @@ colors () {
 	done
 }
 
+git_recent_branches () {
+  git reflog | sed -En 's/.*moving from ([^ ]*) to ([^$]*)/\1 \2/p' | xargs -n 2 printf "%-$((COLUMNS/2-10))s %$((COLUMNS/2-10))s\n"
+}
+
 agent () {
     eval `ssh-agent -s`
     ssh-add
 }
 alias | grep gpa &>/dev/null && unalias gpa
+
