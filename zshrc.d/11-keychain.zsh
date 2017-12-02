f8138a18105e9b9fe9aa111fbf8469be165c94a9
Schlueter <bs@schlueter.blue>
2017-12-02 18:46:41 -0500

    Add keychain start

--- zshrc.d/11-keychain.zsh
+++ zshrc.d/11-keychain.zsh
@@ -2,3 +2,9 @@ GPGKEY=8C4854C3
 GPG_TTY=$(tty)
 export GPGKEY GPG_TTY
 export GPG_AGENT_INFO="~/.gnupg/S.gpg-agent:$(pgrep gpg-agent):1"
+eval $(keychain --eval \
+                --systemd \
+                --timeout $((60*4)) \
+                --nogui \
+                --agents ssh,gpg \
+                id_ed25519 "$GPGKEY")
