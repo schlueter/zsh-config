01d5a94a564df635cd397ae6c010470023a5c54b
Schlueter <bschlueter@gocurrency.com>
2020-01-30 12:08:10 -0800

    Check in completions scripts from brew

--- completions.zsh
+++ completions.zsh
@@ -11,13 +11,7 @@ bashcompinit
     eval "$(kubectl completion zsh)"
 }
 
-for f in "$ZDOTDIR"/completions/_*
+for f in "$ZDOTDIR"/completions/{_*,*.bash}
 do
     source "$f" 2>/dev/null
-done
-
-brew_bash_completions_dir=/usr/local/etc/bash_completion.d
-for f in "$brew_bash_completions_dir"/*
-do
-    source "$f"
 done
