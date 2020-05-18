8a2b78f2e984965f53fbd3f33617554f08fa9f2d
Brandon Schlueter <bschlueter@beachbody.com>
2020-05-18 10:25:56 -0700

    add annotated git log function & alias; comments, spaces in git aliases

--- aliases/git.sh
+++ aliases/git.sh
@@ -1,5 +1,10 @@
+## remote
 alias gRl='git remote -v'
+
+## add
 alias ga='git add'
+alias gap='git add -p'
+
 ## branch
 alias gb='git    branch'
 alias gsupo='git branch --set-upstream-to'
@@ -8,6 +13,7 @@ alias gbr='git   branch -r --no-merged'
 alias gbm='git   branch -r --merged'
 alias gdl=git_delete_branch_local_and_origin
 alias gdl!=git_force_delete_branch_local_and_origin
+
 ## checkout
 alias gco='git  checkout'
 alias gcb='git  checkout -b'
@@ -17,18 +23,23 @@ alias gcml='gcm && git pull'
 alias gcul='git checkout master && git pull upstream master'
 alias gcol=git_checkout_and_pull
 alias gcop=git_checkout_pull_request
+
 ## cherry-pick
 alias gcp='git cherry-pick'
 alias gcpf=git_cherry_pick_file
+
 ## commit
 alias gc='git   commit --gpg-sign --verbose'
 alias gc!='gc   --amend'
 alias gca='gc   --all'
 alias gca!='gc! --all'
+
 ## clone
 alias gcl='git clone --recursive'
+
 ## config
 alias gcf='git config --list'
+
 ## clean
 alias gclean='git clean -fd'
 alias gpristine='git reset --hard && git clean -dfx'
@@ -38,23 +49,30 @@ alias gds='git  diff --stat'
 alias gdc='git  diff --cached'
 alias gdcs='git diff --cached --stat'
 alias gdu='git  diff $(git_primary_branch)'
+
 ## diff-tree
 alias gdt='git  diff-tree --no-commit-id --name-only -r'
+
 ## fetch
 alias gf='git  fetch'
 alias gfa='git fetch --all --prune'
+
 ## log
 alias ghi="git log --graph --decorate=short --date=short --pretty=format:'%C(yellow)%h%C(reset) %C(blue)%ad%C(reset) %C(green)%an%C(reset) %s %C(red)%d%C(reset)'"
+
 ## pull
 alias gl='git  pull'
 alias glu='git pull origin $(gpbb)'
+
 ## push
 alias gp='git push'
 alias gp!='git push -f'
+
 ## branch shortcuts
 alias gip=git_primary_branch
 alias gib=git_current_branch
 alias gipb=git_primary_branch_base
+
 ## rebase
 alias grb='git               rebase'
 alias grba='git              rebase --abort'
@@ -62,22 +80,29 @@ alias grbc='git              rebase --continue'
 alias grbi='git              rebase --interactive'
 alias grbs='git              rebase --skip'
 alias grbu='git fetch && git rebase origin/$(git_primary_branch_base)'
+
 ## reset
 alias grh='git  reset HEAD'
 alias grhh='git reset HEAD --hard'
+
 ## rm
 alias grm='git rm'
+
 ## status
 alias gs='git  status -s'
 alias gsb='git status -sb'
 alias gst='git status'
+
 ## stash
 alias gt='git  stash'
 alias gtd='git stash drop'
 alias gtp='git stash pop'
 alias gts='git stash show'
+alias gtt='git stash save --include-untracked'
+
 ## misc
 alias gre=git-recent-refs
+alias gah=git_annotated_history
 
 ## Hub aliases
 alias gpr='git pull-request'
@@ -152,7 +177,7 @@ git_checkout_pull_request () {
 }
 
 git_delete_branch_local_and_origin () {
-    git branch --delete $1
+    git branch --delete $1 $2
     git push origin :$1
 }
 
@@ -161,5 +186,24 @@ git_force_delete_branch_local_and_origin () {
     git push origin :$1
 }
 
-alias gss='git stash save --include-untracked'
-alias gsp='git stash pop'
+git_annotated_history () {
+    files_in_current_directory="$(find . -mindepth 1 -maxdepth 1 -not \( -wholename '*.git*' -type d \))"
+    filename_width=0
+    export FS='\n'
+    while read -r filename
+    do
+        echo "considering length of $filename"
+        if [ "${#filename}" -gt "$filename_width" ]
+        then
+            filename_width="${#filename}"
+        fi
+    done <$files_in_current_directory
+
+    while read -r filename
+    do
+        echo "printing line for $filename"
+        printf "%-$((filename_width+3))s %s\n" "$filename" "$(
+            git log -n 1 --relative-date --pretty=reference "$filename" || printf 'Untracked'
+        )"
+    done <$files_in_current_directory
+}
