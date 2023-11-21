08c06d7b5805e32beaa1bb19db2b198a7da04bb8
Schlueter <b@schlueter.blue>
2023-11-21 16:02:45 -0500

    Allow gac to accept -p or --profile or assume the first argument is a profile name

--- aliases/misc.sh
+++ aliases/misc.sh
@@ -1,13 +1,17 @@
+#!/usr/bin/env zsh
 gac () {
   declare -a args
   if [ -z "$1" ]
   then
     args+=(--profile "$DEFAULT_AWS_PROFILE")
+  elif [ "$1" = '-p' ] || [ "$1" = '--profile' ]
+  then
+    args+=(--profile "$2")
   else
     args+=(--profile "$1")
   fi
   args+=(
-      --roles /$GAC_ROLE/
+      --roles "/$GAC_ROLE/"
       --mfa-code "$(pass otp "$GAC_PASS_MFA")"
   )
   gimme-aws-creds "${args[@]}"
