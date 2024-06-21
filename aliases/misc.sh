c0fae64c606572ef9f2a40abf3ea4f08d1b1d309
Schlueter <b@schlueter.blue>
2024-06-21 09:39:29 -0400

    Update gimme-aws-creds wrapper
    Signed-off-by: Schlueter <b@schlueter.blue>

--- aliases/misc.sh
+++ aliases/misc.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env zsh
+alias gimme-aws-creds-docker="docker run -it --rm \
+  -v ~/.aws/credentials:/root/.aws/credentials \
+  -v ~/.okta_aws_login_config:/root/.okta_aws_login_config \
+  gimme-aws-creds"
+
 gac () {
   declare -a args
   if [ -z "$1" ]
   then
-    args+=(--profile "$DEFAULT_AWS_PROFILE")
+    args+=(--profile "$GIMME_AWS_CREDS_DEFAULT_AWS_PROFILE")
   elif [ "$1" = '-p' ] || [ "$1" = '--profile' ]
   then
     args+=(--profile "$2")
@@ -11,10 +16,10 @@ gac () {
     args+=(--profile "$1")
   fi
   args+=(
-      --roles "/$GAC_ROLE/"
-      --mfa-code "$(pass otp "$GAC_PASS_MFA")"
+      --roles "/$GIMME_AWS_CREDS_DEFAULT_ROLE/"
+      --mfa-code "$(pass otp "$GIMME_AWS_CREDS_PASS_MFA")"
   )
   gimme-aws-creds "${args[@]}"
 }
-alias otp='pass otp "$OTP_PASS_OTP" -c'
-alias opa='pass "$OPA_PASS" -c'
+alias otp='pass otp "$OKTA_PASS_OTP_NAME" -c'
+alias opa='pass "$OKTA_PASS_NAME" -c'
